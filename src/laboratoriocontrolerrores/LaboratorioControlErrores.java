7921da3520eadd615afa669c4fe030e4e6e1f80f
Steven Iglesias <steveniglesias@hotmail.com>
2017-10-13 18:53:41 -0500

    First Commit
    -Added views
    -Added files support
    -Alert messages

---
+++ src/laboratoriocontrolerrores/LaboratorioControlErrores.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratoriocontrolerrores;
+
+import Interface.MainView;
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Visitante
+ */
+public class LaboratorioControlErrores {
+
+    /**
+     * @param args the command line arguments
+     */
+ 
+        
+    public static Map<String, String> nombreMap = new HashMap<String, String>();
+    public static void main(String[] args) {
+        MainView mainForm= new MainView();
+        mainForm.setVisible(true);
+    }
+
+        
+    
+    
+}
